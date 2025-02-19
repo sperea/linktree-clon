8f746b4d73e30ed8a1de32ed2512640fde4a161d
Sergio Perea <sperea@gmail.com>
2025-02-19 18:25:36 +0100

    carpetas reestructuradas, middleware y servicio de sign-in/up

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 import Image from "next/image";
 
 export default function Home() {
+  const { toast } = useToast();
   return (
     <div>
-      <Button>Click me</Button>
+      <Button
+      onClick={() => {
+        toast({
+          title: "Scheduled: Catch up",
+          description: "Friday, February 10, 2023 at 5:57 PM",
+        })
+      }}
+    >
+      Show Toast
+    </Button>
     </div>
   );
 }
